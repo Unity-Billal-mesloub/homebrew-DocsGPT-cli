3eee9917f257e7f9e1cb97c0a97d809099b0eec6
Pavel <32868631+pabik@users.noreply.github.com>
2026-04-22 13:25:03 +0100

    Update DocsgptCli to version 1.1.1

--- Formula/docsgpt-cli.rb
+++ Formula/docsgpt-cli.rb
@@ -1,9 +1,9 @@
 class DocsgptCli < Formula
   desc "A CLI tool for DocsGPT"
   homepage "https://github.com/arc53/DocsGPT-cli"
-  url "https://github.com/arc53/DocsGPT-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b9f18862142a6ff9536dd272bf9d6fe01401201a77cbec34275d2a9a05f7ed4f"
-  version "1.0.0"
+  url "https://github.com/arc53/DocsGPT-cli/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "435e4e571d43fa8ddcd13e4824cc01afe05eac807892f8ce4bfd7a562dc93e0d"
+  version "1.1.1"
   license "MIT"
 
   depends_on "go" => :build
